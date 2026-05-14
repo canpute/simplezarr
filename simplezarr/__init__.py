c437ae3f715adcdbea58a61315a3bf370252c123
Caitlin Lewis <caitlin9165@gmail.com>
2026-05-14 09:03:23 -0400

    add slow store (#11)
    * add slow store
    
    * add tests, fix bug
    
    * requested changes

--- simplezarr/__init__.py
+++ simplezarr/__init__.py
@@ -1,5 +1,5 @@
 # ruff: noqa: F401
 
 from .stores import BaseStore, ReadableStore, WritableStore, ListableStore
-from .stores import LocalStore
+from .stores import LocalStore, SlowStore
 from .nodes import open_zarr, ZarrNode, ZarrGroup, ZarrArray
